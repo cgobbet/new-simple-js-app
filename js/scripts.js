246730adc07ed790863c39abcedb98b9abdb4555
[cgobbet] <[cassiano.gobbet@gmail.com]>
2019-09-29 19:22:41 +0200

    Update scripts.js
    getAll and addListItem functions

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,25 @@ var pokemonRepository = (function () {
       alert("This is not an object");
     }
   }
+  
+  function getAll() {
+    return repository;
+  }
+
+  function addListItem(pokemon) {
+    var $pokemonList = $('.pokemon-list');
+    var $pokemonListItem = $('li');
+    var $pokemonButton = $('button');
+
+    $pokemonButton.innerText = pokemon.name;
+
+    $pokemonButton.addClass('button-class');
+    $pokemonListItem.addClass('pokemon-list__item');
+
+    $('$pokemonButton').appendTo('$pokemonListItem');
+    $('$pokemonListItem').appendTo('$pokemonList');
+
+    $('$pokemonButton').click(function(){
+      showDetails(pokemon);
+    });
+  }
