5db58d3ecdefbf09e74ceb01485b11e4bdea6037
[cgobbet] <[cassiano.gobbet@gmail.com]>
2019-10-01 15:26:39 +0200

    Update scripts.js
    Update of list size

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var pokemonRepository = (function() {
   var repository = [];
-  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=200';
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=40';
   var $modalContainer = $('#modal-container');
 
   function add(pokemon) {
