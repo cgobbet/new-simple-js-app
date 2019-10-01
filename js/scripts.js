53fa4316f7e3663034f869078c64418c9b29cfdc
[cgobbet] <[cassiano.gobbet@gmail.com]>
2019-10-01 18:53:14 +0200

    Update scripts.js
    Pokemon number change

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var pokemonRepository = (function() {
   var repository = [];
-  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=40';
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   var $modalContainer = $('#modal-container');
 
   function add(pokemon) {
